cba0a9483e0b85d292f045025d2033d2c98007d0
lbroudoux <laurent.broudoux@gmail.com>
2019-08-20 19:12:42 +0200

    Adding streaming of quote changes

--- quotegame-api/src/main/java/com/redhat/quotegame/QuoteGameApp.java
+++ quotegame-api/src/main/java/com/redhat/quotegame/QuoteGameApp.java
@@ -8,6 +8,7 @@ import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
 import org.apache.kafka.common.errors.TimeoutException;
+import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,7 +33,13 @@ public class QuoteGameApp {
         logger.info("Create or get caches named quotegame-users, quotegame-portfolios with the default configuration");
         cacheManager.administration().getOrCreateCache("quotegame-users", "default");
         cacheManager.administration().getOrCreateCache("quotegame-portfolios", "default");
-
+        
+        RemoteCache<String, Double> quotesCache = cacheManager.administration().getOrCreateCache("quotegame-quotes", "default");
+        // Put initialization values if empty.
+        if (quotesCache.isEmpty()) {
+            quotesCache.put("RHT", 187.71);
+            quotesCache.put("IBM", 140.57);
+        }
         waitUntilStarted.countDown();
     }
 
