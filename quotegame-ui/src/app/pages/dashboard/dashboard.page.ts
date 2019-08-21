db63ce5323afb04b5b160b080fbe01ebf60930de
lbroudoux <laurent.broudoux@gmail.com>
2019-08-21 19:12:00 +0200

    Adding quote charts !

--- quotegame-ui/src/app/pages/dashboard/dashboard.page.ts
+++ quotegame-ui/src/app/pages/dashboard/dashboard.page.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 
 import { CardConfig } from 'patternfly-ng/card';
+import { SparklineChartConfig, SparklineChartData } from 'patternfly-ng/chart';
 
 import { BackendService } from '../../services/backend.service';
 import { AuthenticationService } from '../../services/auth.service';
@@ -15,22 +16,38 @@ export class DashboardPageComponent implements OnInit {
   private username: string;
   private email: string;
 
-  chartCardConfig: CardConfig;
+  fiveSecLong: number = (1000 * 5);
+  numberOfTicks: number = 30;
+  today = new Date();
 
-  /*
+  rhChartCardConfig: CardConfig;
+  ibmChartCardConfig: CardConfig;
+
   chartDates: any[] = ['dates'];
-  chartConfig: SparklineChartConfig = {
-		chartId: 'quotesSparkline',
+
+  rhChartConfig: SparklineChartConfig = {
+		chartId: 'rhQuotesSparkline',
 		chartHeight: 150,
 		tooltipType: 'default'
   };
-  chartData: SparklineChartData = {
+  ibmChartConfig: SparklineChartConfig = {
+		chartId: 'ibmQuotesSparkline',
+		chartHeight: 150,
+		tooltipType: 'default'
+  };
+
+  rhChartData: SparklineChartData = {
     dataAvailable: false,
     total: 100,
     xData: this.chartDates,
     yData: ['used']
   };
-  */
+  ibmChartData: SparklineChartData = {
+    dataAvailable: false,
+    total: 100,
+    xData: this.chartDates,
+    yData: ['used']
+  };
 
   constructor(protected authService: AuthenticationService, protected backendService: BackendService) { }
 
@@ -38,26 +55,59 @@ export class DashboardPageComponent implements OnInit {
     if (this.authService.isAuthenticated()) {
       //this.loadStatus();
     }
+    this.connectQuoteStream();
+    this.rhChartCardConfig = { title: 'RHT Stock' } as CardConfig;
+    this.ibmChartCardConfig = { title: 'IBM Stock' } as CardConfig;
 
-    this.chartCardConfig = {
-      action: {
-      },
-      filters: [{
-				title: 'Last 5 Hours',
-        value: '5'
-      }, {
-				default: true,
-        title: 'Last 2 Hours',
-        value: '2'
-      }, {
-        title: 'Last Hour',
-        value: '1'
-      }],
-      title: 'RHT Stock',
-    } as CardConfig;
+    // Initialize charts data.
+    this.rhChartData.dataAvailable = false;
+    this.ibmChartData.dataAvailable = false;
+    this.rhChartData.xData = ['dates'];
+    this.ibmChartData.xData = ['dates'];
+    this.rhChartData.yData = ['dollars'];
+    this.ibmChartData.yData = ['dollars'];
+
+    for (let i = this.numberOfTicks - 1; i >= 0; i--) {
+      var pastDate: Date = new Date(this.today.getTime() - (i * this.fiveSecLong));
+      this.rhChartData.xData.push(pastDate);
+      this.ibmChartData.xData.push(pastDate);
+      this.rhChartData.yData.push(0);
+      this.ibmChartData.yData.push(0);
+    }
+    this.rhChartData.dataAvailable = true;
+    this.ibmChartData.dataAvailable = true;
   }
 
   public isAuthenticated(): boolean {
     return this.authService.isAuthenticated();
+  }
+
+  connectQuoteStream(): void {
+    this.backendService.getQuoteStreaming().subscribe(
+      results => {
+        //console.log("Message: " + JSON.stringify(results.data));
+        var now = new Date();
+        const quotes = JSON.parse(results.data);
+
+        this.rhChartData.dataAvailable = false;
+        this.ibmChartData.dataAvailable = false;
+
+        quotes.forEach(quote => {
+          if (quote.symbol === 'RHT') {
+            this.rhChartData.xData.push(now);
+            this.rhChartData.yData.push(quote.price);
+            this.rhChartData.xData.splice(1, 1);
+            this.rhChartData.yData.splice(1, 1);
+          } else if (quote.symbol === 'IBM') {
+            this.ibmChartData.xData.push(now);
+            this.ibmChartData.yData.push(quote.price);
+            this.ibmChartData.xData.splice(1, 1);
+            this.ibmChartData.yData.splice(1, 1);
+          }
+        });
+        this.rhChartData.dataAvailable = true;
+        this.ibmChartData.dataAvailable = true;
+      }    
+    );
   }
 }
--- quotegame-ui/src/app/services/backend.service.ts
+++ quotegame-ui/src/app/services/backend.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -7,4 +7,24 @@ export class BackendService {
 
   private rootUrl: string = '/api';
 
+  constructor(private zone: NgZone) {}
+
+  public getQuoteStreaming(): Observable<any> {
+    console.log("Creating observer");
+    return Observable.create(observer => {
+      const eventSource = new EventSource(this.rootUrl + "/quote/streaming");
+
+      eventSource.onmessage = event => {
+        this.zone.run(() => {
+          observer.next(event);
+        })
+      };
+      eventSource.onerror = event => {
+        this.zone.run(() => {
+          observer.error(event);
+        })
+      };
+      return () => eventSource.close();
+    });
+  }
 }
