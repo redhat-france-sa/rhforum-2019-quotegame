31652a3015904aaf8ead4774f6c10edbd88f4c23
lbroudoux <laurent.broudoux@gmail.com>
2019-09-17 12:54:41 +0200

    Manually merging PR #1

--- quotegame-processors/src/main/java/com/redhat/quotegame/processors/QuotePriceUpdater.java
+++ quotegame-processors/src/main/java/com/redhat/quotegame/processors/QuotePriceUpdater.java
@@ -2,6 +2,7 @@ package com.redhat.quotegame.processors;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import com.redhat.quotegame.model.Order;
 import com.redhat.quotegame.model.Quote;
@@ -11,6 +12,9 @@ import org.infinispan.client.hotrod.RemoteCache;
 import org.jboss.logging.Logger;
 
 import io.quarkus.infinispan.client.runtime.Remote;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+import org.kie.kogito.rules.KieRuntimeBuilder;
 
 @ApplicationScoped
 /**
@@ -21,6 +25,14 @@ public class QuotePriceUpdater {
  
     private final Logger logger = Logger.getLogger(getClass());
     
+    private KieSession ksession;
+
+    @Inject
+    @Named("quotePriceKS")
+    QuotePriceUpdater( KieRuntimeBuilder runtimeBuilder ) {
+        ksession = runtimeBuilder.newKieSession();
+    }
+
     @Inject
     @Remote("quotegame-quotes")
     RemoteCache<String, Quote> quotesCache;
@@ -29,5 +41,30 @@ public class QuotePriceUpdater {
     public void computeQuotePrices(Order order) {
         logger.info("Get new order to process...");
 
+        logger.info("Get corresponding Quote ...");
+        Quote quote = quotesCache.get(order.getQuote());
+        int quoteBefore = quote.hashCode();
+
+        logger.info("Inserting Order fact ...");
+        ksession.insert(order);
+
+        logger.info("Inserting Quote fact ...");
+        FactHandle quoteFH = ksession.insert(quote);
+
+        logger.info("Fire Rules ...");
+        int rulesFired = ksession.fireAllRules();
+        logger.info("Number of rules fired = " + rulesFired);
+
+        int quoteAfter = quote.hashCode();
+
+        if (quoteBefore == quoteAfter){
+            logger.info("Quote not modified");
+        } else {
+            logger.info("Updating Quote cache");
+            quotesCache.replace(order.getQuote(), quote);
+        }
+
+        ksession.delete(quoteFH);
+        logger.info("Number of facts in WM = " + ksession.getFactCount());
     }
 }
