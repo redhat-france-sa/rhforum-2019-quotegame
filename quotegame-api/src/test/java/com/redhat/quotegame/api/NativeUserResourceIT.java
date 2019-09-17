173862715ede4e84f2563269b71d004be70f0cdc
lbroudoux <laurent.broudoux@gmail.com>
2019-09-17 18:26:55 +0200

    Upgrading Quarkus and building native images

--- quotegame-api/src/test/java/com/redhat/quotegame/api/NativeUserResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.quotegame.api;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeUserResourceIT extends UserResourceTest {
-
-    // Execute the same tests but in native mode.
-}
